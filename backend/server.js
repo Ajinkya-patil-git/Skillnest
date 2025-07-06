60f666e2490d043f307987ea9dc7c497eaeb4b16
ajink <ajinkyapa48@gmail.com>
2025-07-06 16:53:30 +0530

    Fix CORS configuration for frontend domain

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,11 @@ app.use(express.json()); // to parse json body
 app.use(cookieParser());
 app.use(
     cors({
-        // origin: 'http://localhost:5173', // frontend link
-        origin: "*",
-        credentials: true
+        origin: ["https://skillnest-v5p6-ph3cwclh0-ajinkyapa48-6393s-projects.vercel.app", "http://localhost:5173"],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'x-csrf-token'],
+        exposedHeaders: ['*', 'Authorization']
     })
 );
 app.use(
